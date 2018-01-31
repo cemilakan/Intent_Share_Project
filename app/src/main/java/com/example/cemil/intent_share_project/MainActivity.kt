d6bfbba9c418b44f4e6d9634869cc3db4084f450
cemilakan <35969240+cemilakan@users.noreply.github.com>
2018-01-31 17:43:23 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/cemil/intent_share_project/MainActivity.kt
+++ app/src/main/java/com/example/cemil/intent_share_project/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.cemil.intent_share_project
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun textShare(view: View) {
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.putExtra(Intent.EXTRA_STREAM, messageText.text.toString())
+        shareIntent.type = "text/plain"//intente ne türde veri göndereceğimizi beirtiyoruz.
+        startActivity(Intent.createChooser(shareIntent,"Nasıl Paylaşmak İstersiniz?"))
     }
 }
